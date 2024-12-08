a651b323c06a4943b3ed389b578098de6c442640
Ujjwal Ranjan Choubey <ranjanujjwal980@gmail.com>
2024-12-08 16:25:42 +0530

    first commit

---
+++ src/environment.js
@@ -0,0 +1,10 @@
+let IS_PROD = true;
+const server = IS_PROD ?
+      "https://backend-videocall-app.onrender.com" :
+          "http://localhost:8000"
+    // "https://apnacollegebackend.onrender.com" :
+
+    
+
+
+export default server;
