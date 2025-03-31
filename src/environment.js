73027965ace36253bf48f1603abef33e5b74fe6f
Ujjwal Ranjan Choubey <129257114+Ujjwal1267@users.noreply.github.com>
2025-03-31 23:33:48 +0530

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-      "https://video-call-1-stjz.onrender.com" :
+      "https://backend-videocall-app.onrender.com" :
           "http://localhost:8000"
     // "https://apnacollegebackend.onrender.com" :
 
